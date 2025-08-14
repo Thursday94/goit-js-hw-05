386b15f6c3c02d1d7e0f8edf8854801e88766af3
Roman Kovalov <romankovslov94@gmail.com>
2025-08-14 18:04:16 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+function calcAverageCalories(days) {
+    let totalCalories = 0;
+    let count = 0;
+    for (const day of days) {
+      totalCalories += day.calories;
+      count++;
+    }
+    return totalCalories / count;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+);
